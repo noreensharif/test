47aa99b756648f162fb97d64091c124a15493287
noreensharif <noreensharif123@yahoo.com>
2024-01-18 14:50:29 -0800

    push js
    undefined

---
+++ array.js
@@ -0,0 +1,55 @@
+let a=[1,2,3,4];
+//{
+    // console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+// console.log(a[3]);
+// console.log(a[4]);
+// console.log(a[-1]);
+// for (let index = 0; index < a.length; index++) {
+//    console.log(a[index]);
+    
+// }
+//in map function x=a[index]
+// a.map( (x,index)=>{
+//     //console.log(x);
+//     console.log(a[index])})
+
+
+
+
+
+// function func1(a){
+//     return a;
+
+// }
+
+// let name1=func1('noreen')
+// console.log(name1)
+
+// console.log(func1('abc'))
+
+// let func1=(a)=>{ arrow function
+// return(a)
+// }
+
+
+// ()=>{   //annonymous function
+//     console.log('hussain');
+// };//
+//}
+
+// let b=a.filter( (x)=>{return x%2==0});
+// console.log(b);
+// let limit=3;
+
+// a.slice(0,limit).map(x=>console.log(x))
+
+
+// limit=5;
+// a.slice(0,limit).map(x=>console.log(x))
+
+
+//console.log(a)
+
+console.log(a.findIndex(x=>{ return x==4}))
