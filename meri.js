5beeb07cedfc796696ffbe99fa3c99369403e3fa
noreensharif <noreensharif123@yahoo.com>
2024-01-18 12:22:12 -0800

    first commit

---
+++ meri.js
@@ -0,0 +1,10 @@
+function navigateTo(abc){
+    window.location.href=abc;
+}
+
+// function myJSX(){
+//     let name='ya allah'
+//     return <div>
+//         <h1>Alhumdillah ${name}</h1>
+//     </div>
+// }
---
+++ simple.js
@@ -0,0 +1,1 @@
+console.log('Shukar Ya Allah');
